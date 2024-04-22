4a5c4eb9550d5e887bf4dc66f0eb73f990ff5086
A. H. M NAYEM HOSSAIN <ahmnayemhossain@gmail.com>
2024-04-22 15:45:12 +0600

    Create Simple_Navigation.dart

---
+++ Simple_Navigation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HomeActivity(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+        }, child: Text('Go Activity 1')),
+        ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2()));
+        }, child: Text('Go Activity 2'))
+      ],
+    )
+    );
+  }
+}
+
+class Activity1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Activity 1'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity2()));
+        },child: Text('Go to Activity 2'),),
+      ),
+    );
+  }
+}
+
+class Activity2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Activity 2'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>Activity1()));
+        },child: Text('Go to Activity 1'),),
+      ),
+    );
+  }
+}

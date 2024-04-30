b70aa9fce749fec6e7607dc219980f58a449cbf8
A. H. M NAYEM HOSSAIN <ahmnayemhossain@gmail.com>
2024-04-30 17:59:05 +0600

    Create Circle_progress_Bar.dart

---
+++ Circle_progress_Bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HomeActivity(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    body: Center(
+      child: CircularProgressIndicator(
+        color: Colors.amber,
+      )
+    )
+    );
+  }
+}

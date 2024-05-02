84374684f03e486ca30ea63e80fa8b6c9fbd00c8
A. H. M NAYEM HOSSAIN <ahmnayemhossain@gmail.com>
2024-05-02 22:37:19 +0600

    Create Fractionaly_Sized_Box.dart

---
+++ Fractionaly_Sized_Box.dart
@@ -0,0 +1,42 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: HomeActivity(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    body: Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        heightFactor: 0.3,
+        child: Container(
+          color: Colors.amber,
+        ),
+      ),
+    )
+    );
+  }
+}
